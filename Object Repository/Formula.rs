1a69beb6365f5ba93df46ce5dbab84979cda24eb
Lê Quang Duy - CMC Global DU8 <lqduy@ldap.cmcglobal.com.vn>
2020-10-27 14:18:27 +0700

    first init

---
+++ Object Repository/Formula.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>Formula</name>
+   <tag></tag>
+   <elementGuidId>05772c5b-b4e8-4aa9-9f07-841e25536dfe</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>com.sec.android.app.popupcalculator:id/calc_edt_formula</locator>
+   <locatorStrategy>ID</locatorStrategy>
+</MobileElementEntity>

46097aa2686ebcb444ff1d70d276fb301f8435c7
lqduy <lqduy@LQDUY.ldap.cmcglobal.com.vn>
2020-11-03 16:29:03 +0700

    update drag and drop function

--- Keywords/swipe.groovy
+++ Keywords/swipe.groovy
@@ -2,33 +2,33 @@ import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 
 public class swipe {
-	
+
 	int xLeft = Mobile.getDeviceWidth() * 0.3;
 	int xRight = Mobile.getDeviceWidth() * 0.7;
 
-	int yUp = Mobile.getDeviceHeight() * 0.7;
-	int yDown = Mobile.getDeviceHeight() * 0.3;
+	int yUp = Mobile.getDeviceHeight() * 0.3;
+	int yDown = Mobile.getDeviceHeight() * 0.7;
 
 	int midX = Mobile.getDeviceWidth() * 0.5;
 	int midY = Mobile.getDeviceHeight() * 0.5;
 	@Keyword
 	public void swipeLeft() {
-		
+
 		Mobile.swipe(xLeft, midX,xRight, midX)
 	}
 	@Keyword
 	public void swipeRight() {
-		
+
 		Mobile.swipe(xRight, midX, xLeft, midX)
 	}
 	@Keyword
-	public void swipeUp() {
-		
+	public void swipeDown() {
+
 		Mobile.swipe(midX, yDown, midX, yUp)
 	}
 	@Keyword
-	public void swipeDown() {
-		
+	public void swipeUp() {
+
 		Mobile.swipe(midX, yUp, midX, yDown)
 	}
 }
--- Scripts/General Management/Drag and Drop Language/Script1603877675935.groovy
+++ Scripts/General Management/Drag and Drop Language/Script1603877675935.groovy
@@ -1,34 +1,26 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory as MobileDriverFactory
 import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
+
+import io.appium.java_client.MobileBy as MobileBy
+import io.appium.java_client.TouchAction
+import io.appium.java_client.android.AndroidElement
+import io.appium.java_client.touch.offset.PointOption
 
 Mobile.scrollToText('General Management')
 
-Mobile.tap(findTestObject('Common/Main Option', [('optionName'):'General management']), 0)
+CustomKeywords.'gesture.tap'(findTestObject('Common/Main Option', [('optionName') : 'General management']))
 
-Mobile.tap(findTestObject('Common/Main Option', [('optionName'):'Language and input']), 0)
+CustomKeywords.'gesture.tap'(findTestObject('Common/Main Option', [('optionName') : 'Language and input']))
 
-Mobile.waitForElementPresent(findTestObject('Common/Main Option', [('optionName'):'Language']), 3)
+CustomKeywords.'gesture.tap'(findTestObject('Common/Main Option', [('optionName') : 'Language']))
 
-Mobile.tap(findTestObject('Common/Main Option', [('optionName'):'Language']), 0)
+TestObject source = findTestObject('General Management/Language and input/Language/Language', [('language') : 'Viet'])
 
-TestObject fromObject = findTestObject('General Management/Language and input/Language/Language', [('language'):'Viet']).
+TestObject target = findTestObject('General Management/Language and input/Language/Language', [('language') : 'English'])
 
-TestObject toObject = findTestObject('General Management/Language and input/Language/Language', [('language'):'English'])
+CustomKeywords.'gesture.dragAndDrop'(source, target)
 
-Mobile.dragAndDrop(fromObject, toObject, 3)
+Mobile.delay(3)
